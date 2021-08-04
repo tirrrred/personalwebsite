3666da6dc3f1be4e95a0592832a0456bf5a640ed
Tord Langedal Ness <tordness@gmail.com>
2021-08-04 13:39:22 +0200

    new content in about-me

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -6,10 +6,16 @@
           Welcome!<br><span class="contrast-blue">Good to see you!</span><br><p>Who am I, you might ask? Keep reading and I will show you</p>
         </b-container>
         <b-container class="text-block">
-          But first, a short <span class="contrast-blue">pitch</span>:<br><br>
-          29 year old aspiring technologist who love the intersection between business, people and technology, with operational experience from the <a href="https://www.forsvaret.no/en/organisation/norwegian-cyber-defence" target="_blank">Norwegian Armed Forces Cyber Defence</a>,
-          contribution in both domestic and <a href="https://www.forsvaret.no/en/exercises-and-operations/international-operations" target="_blank">international operations</a> before designing, implementing and operating enterprise IT infrastructure<br><br><br>
-          <p class="small"><span class="contrast-blue">psst:</span> <a href="#" target="_blank" rel="noopener noreferrer">Here is the TL;DR (also known as CV) for the lazy people (like me)</a></p>
+          But in first <span class="contrast-blue">short</span>:<br><br>
+          I love technology and people and how those two combined can provide business value. My goal is to create strong teams and organizations by exploiting the 
+          synergy between tech, people, and business.<br><br>
+          I currently lead an IT infrastructure team of 15 people responsible for secure and stable operations of critical financial systems.<br><br>
+          Operational experience from the <a href="https://www.forsvaret.no/en/organisation/norwegian-cyber-defence" target="_blank">Norwegian Armed Forces Cyber Defence</a>,
+          contribution in both domestic and <a href="https://www.forsvaret.no/en/exercises-and-operations/international-operations" target="_blank">international operations</a>, 
+          and experience at designing, implementing, and operating enterprise infrastructure have provided a solid technical foundation. Leadership roles in both the 
+          Armed Forces and DNB have sparked my love for leadership and people.
+          <br><br>
+          <p class="small"><span class="contrast-blue">psst:</span> <a href="#" rel="noopener noreferrer">Here is the TL;DR version for the lazy people (like me)</a></p>
         </b-container>
       </b-container>
     </b-container>
@@ -19,11 +25,13 @@
           WOW, your still here <span class="contrast-blue"><br>Amazing!</span><br><p>Let me tell you a <span class="contrast-blue">story</span></p>
         </b-container>
         <b-container class="text-block">
-          Once upon a time, in a <a href="https://www.eikefjord.net/" target="_blank" rel="noopener noreferrer">small, idyllic village</a> at the west coast of Norway, I small boy enjoyed the unconcerned and easy life with family and friends. Although several activities took alot of his time, 
-          nothing could compare with the interest for <span class="contrast-blue">football</span> (the real type of football, not the kind they play in the US). It took most of his time, and he found a great love for the sport. To be honest, he was mostly focusing on having a good time with friends and playing footbal, 
-          and fortunately those two could be combined, as good friends also played football. When it came to school, it was a scoial arena to meet friends and also have a good time. The academic results was not of interest, and more of a necessity to be done. Ufortunatley for the boy, 
-          reality was lurking around the corner, and inevitble decisions about his future were needed to be faced. Without any clear plan for his career, he stumpled upon a oppurtuiniy to live out a classic small boy dream, and at the same time get a relevant and interesting education;
-          <a href="https://www.forsvaret.no/utdanning/utdanninger/ingeniorfag-studieretning-telematikk" target="_blank" rel="noopener noreferrer"><span class="contrast-blue">the military</span></a>
+          Once upon a time, in a <a href="https://www.eikefjord.net/" target="_blank" rel="noopener noreferrer">small, idyllic village</a> on the west coast of Norway, 
+          a small boy enjoyed the unconcerned and easy life with family and friends. As for most boys, it was an active childhood where several different sports took place, 
+          but nothing could compare with the interest in <span class="contrast-blue">football</span>. All the time used to play football ensured an active, healthy, social boy, 
+          who loved the sense of working in a team and achieving results with the team. This was the life; no worries, only football, and happiness! Unfortunately for the boy, 
+          the reality was lurking around the corner, and inevitable decisions about his future were needed to be faced. Without any clear plan for his career, 
+          he stumbled upon an opportunity to live out a classic boy's dream, and at the same time, get a relevant and exciting education;
+          <a href="https://www.forsvaret.no/utdanning/utdanninger/ingeniorfag-studieretning-telematikk" target="_blank" rel="noopener noreferrer"><span class="contrast-blue">the military</span></a>.<br><br>
         </b-container>
       </b-container>
     </b-container>
@@ -33,23 +41,22 @@
           Character and <span class="contrast-blue">ambitions</span><br><p>A new chapter</p>
         </b-container>
         <b-container class="text-block">
-          Befalsskole + ingeniør <br>
-          øvelser + kammeratskap <br>
-          Karakter, ståpåvilje, mestring, lederskap
+          What a transition! The boy needed to adjust from the hassle-free time at high school to the result-oriented Norwegian Defence Cyber Academy. Not only did the academy 
+          follow regular bachelor's degree education and courses, but it also provided military leadership and skills training. The days and evenings were structured and packed 
+          with subjects and content. One day could be pure academic with engineering subjects, while the next day where a full-blown military exercise with demanding leadership 
+          and personal challenges. Although the transition was rough, the boy loved it! The demanding environment, the individual and leadership development courses, the long hours, 
+          both at the school bench and in the woods, the camaraderie, the teamwork - it all fueled his personality with energy and new joy. In this process, his new character was built, 
+          and with it, higher aspiration and ambitions. Oh, by the way, he still got time to play football in his spare time, although not as much as previously.
         </b-container>
       </b-container>
     </b-container>
     <b-container fluid class="row-background-cover experienceMe">
       <b-container class="info-block">
         <b-container class="text-block tb-header">
-          What do I like?
+          New challenges
         </b-container>
         <b-container class="text-block">
-          Starting in the 
-          A 29 year old, aspiring technologist who love the intersection between business, people and technology<br>
-          Total of 7 years experience in IT, starting in the <a href="https://www.forsvaret.no/en/organisation/norwegian-cyber-defence" target="_blank">Norwegian Armed Forces Cyber Defence</a>,
-          contribution in both domestic and <a href="https://www.forsvaret.no/en/exercises-and-operations/international-operations" target="_blank">international operations</a><br>
-          Have profound experience in IT architecture, designing IT infrastructure, as well as implementation of techincal changes and operating and maintaining IT infrastructure
+          DNB
         </b-container>
       </b-container>
     </b-container>
